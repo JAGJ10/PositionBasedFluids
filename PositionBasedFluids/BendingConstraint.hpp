2dc75e753a5b0aa79989409ae6370b48fdbcde55
Joel <jagj10@gmail.com>
2015-03-07 16:09:30 -0500

    bugfix

--- PositionBasedFluids/BendingConstraint.hpp
+++ PositionBasedFluids/BendingConstraint.hpp
@@ -12,7 +12,7 @@ public:
 	float restLength;
 	float w;
 
-	BendingConstraint(Particle *p1, Particle *p2, Particle *p3, Particle *p4) : p1(p1), p2(p2), p3(p3) {
+	BendingConstraint(Particle *p1, Particle *p2, Particle *p3) : p1(p1), p2(p2), p3(p3) {
 		glm::vec3 center = (1 / 3) * (p1->oldPos + p2->oldPos + p3->oldPos);
 		restLength = glm::length(p3->oldPos - center);
 		w = p1->invMass + p2->invMass + (2 * p3->invMass);
